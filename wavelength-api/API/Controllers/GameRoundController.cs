9d202793a09b1d661f61d2859b4297e61c306577
Peyton Peck <peckpeyton1@hotmail.com>
2024-02-27 20:38:57 -0600

    Add first GameRound endpoint

---
+++ wavelength-api/API/Controllers/GameRoundController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class GameRoundController : BaseAPIController
+{
+    [Authorize(Policy = "IsGhostOnTeamTurn")]
+    [HttpPost("performGhostGuess")]
+    public async Task<ActionResult> PerformGhostGuess()
+    {
+        return null;
+    }
+}
