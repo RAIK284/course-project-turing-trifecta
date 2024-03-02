1fa07a085806a62bfa1263fab0911371a10409e0
nbray03 <112013865+nbray03@users.noreply.github.com>
2024-03-01 20:09:46 -0600

    Finished the 3 endpoints
    Finished SelectTarget, PerformOpposingTeamGuess, and PerformOpposingTeamSelection

--- wavelength-api/API/Controllers/GameRoundController.cs
+++ wavelength-api/API/Controllers/GameRoundController.cs
@@ -7,10 +7,35 @@ namespace API.Controllers;
 
 public class GameRoundController : BaseAPIController
 {
+    // perform the ghost guess
     [Authorize(Policy = "IsGhostOnTeamTurn")]
     [HttpPost("performGhostGuess")]
     public async Task<ActionResult> PerformGhostGuess([FromBody] GameRoundGhostGuessDTO param)
     {
         return HandleResult(await Mediator.Send(new PerformGhostGuess.Command(param)));
+    }
+
+    // select the target for the 'selector' role
+    [Authorize(Policy = "IsSelectorOnTeamTurn")]
+    [HttpPost("selectTarget")]
+    public async Task<ActionResult> SelectTarget([FromBody] GameRoundSelectorSelectionDTO param)
+    {
+        return HandleResult(await Mediator.Send(new SelectTarget.Command(param)));
+    }
+
+    // Perform opposing team guess
+    [Authorize(Policy = "IsGhostOnOpposingTeam")]
+    [HttpPost("performOpposingTeamGuess")]
+    public async Task<ActionResult> PerformOpposingTeamGuess([FromBody] GameRoundOpposingTeamGuessDTO param)
+    {
+        return HandleResult(await Mediator.Send(new PerformOpposingTeamGuess.Command(param)));
+    }
+
+    // Perform opposing team selection
+    [Authorize(Policy = "IsSelectorOnOpposingTeam")]
+    [HttpPost("performOpposingTeamSelection")]
+    public async Task<ActionResult> PerformOpposingTeamSelection([FromBody] GameRoundOpposingTeamSelectionDTO param)
+    {
+        return HandleResult(await Mediator.Send(new PerformOpposingTeamSelection.Command(param)));
     }
 }
