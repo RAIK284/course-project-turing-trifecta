151ef86f8e6893548491078135579dbc76e2376d
Peyton Peck <peckpeyton1@hotmail.com>
2024-03-04 10:48:03 -0600

    Change app text color to white

--- wavelength/src/App.tsx
+++ wavelength/src/App.tsx
@@ -5,7 +5,7 @@ import AppBackground from "./components/AppBackground";
 
 const App: React.FC = () => {
   return (
-    <div className="App bg-theme-blue w-screen h-screen fixed">
+    <div className="App bg-theme-blue w-screen h-screen fixed text-white">
       <AppBackground />
       <NavBar />
       {/* Children passed into the app in Routes.tsx will be output here */}
