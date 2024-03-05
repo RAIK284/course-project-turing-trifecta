29464242e9cf63cf0b1afa0e393a4506abdd1989
Peyton Peck <notsizzle@gmail.com>
2024-03-05 14:53:24 -0600

    Revert "Update vite config"
    This reverts commit 5267b97739f54844bd41244704ed03eee6206ced.

--- wavelength/vite.config.ts
+++ wavelength/vite.config.ts
@@ -3,16 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: "APP_",
-  server: {
-    port: 3000,
-    host: "127.0.0.1",
-  },
-  preview: {
-    port: 3000,
-  },
   plugins: [react()],
-  build: {
-    target: ["chrome89", "edge89", "firefox89", "safari15"],
-  },
+  envPrefix: "APP_",
 });
