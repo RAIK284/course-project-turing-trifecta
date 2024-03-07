ded1bace7541dd4613c1e15a91ab2627ea6c764e
jakeRance <jake.rance01@gmail.com>
2024-03-07 16:26:17 -0600

    Made the unathenticated home page

--- wavelength/src/pages/UnauthenticatedLandingPage.tsx
+++ wavelength/src/pages/UnauthenticatedLandingPage.tsx
@@ -1,16 +1,26 @@
+import { useNavigate } from "react-router";
+import { WavelengthPath } from "../routing/Routes";
+
+
+
 const UnauthenticatedLandingPage: React.FC = () => {
+  const navigate = useNavigate();
+
   function handleLoginClick(){
-    console.log("Login")
+    navigate(WavelengthPath.LOGIN);
   }
 
   function handleRegisterClick(){
-    console.log("Register")
+    navigate(WavelengthPath.REGISTER);
   }
 
   return(
-    <div className="bg-theme-blue">
-      <button className="bg-cover-blue" onClick={handleLoginClick}>Login</button>
-      <button className="bg-target-2" onClick={handleRegisterClick}>Register</button>
+    <div className="h-screen flex justify-center self-center space-y-1/2">
+      <div className="grid grid-rows h-1/3 w-1/3 my-auto">
+        <button className="bg-cover-blue h-16" onClick={handleLoginClick}>Login</button>
+        <p className="justify-self-center">or</p>
+        <button className="bg-target-2 h-16" onClick={handleRegisterClick}>Register</button>
+      </div>
     </div>
   );
 };
