b72ea24b74a6b75b725ff61263d9c9c87d0c2207
jakeRance <jake.rance01@gmail.com>
2024-03-07 22:40:30 -0600

    Cleaned up some font issues

--- wavelength/src/pages/UnauthenticatedLandingPage.tsx
+++ wavelength/src/pages/UnauthenticatedLandingPage.tsx
@@ -1,8 +1,6 @@
 import { useNavigate } from "react-router";
 import { WavelengthPath } from "../routing/Routes";
 
-
-
 const UnauthenticatedLandingPage: React.FC = () => {
   const navigate = useNavigate();
 
@@ -17,9 +15,9 @@ const UnauthenticatedLandingPage: React.FC = () => {
   return(
     <div className="h-screen flex justify-center self-center space-y-1/2">
       <div className="grid grid-rows h-1/3 w-1/3 my-auto">
-        <button className="bg-cover-blue h-16" onClick={handleLoginClick}>Login</button>
-        <p className="justify-self-center">or</p>
-        <button className="bg-target-2 h-16" onClick={handleRegisterClick}>Register</button>
+        <button className="bg-cover-blue h-16 md:text-3xl font-sans" onClick={handleLoginClick}>LOGIN</button>
+        <p className="justify-self-center md:text-2xl font-sans">or</p>
+        <button className="bg-target-2 h-16 md:text-3xl font-sans" onClick={handleRegisterClick}>CREATE ACCOUNT</button>
       </div>
     </div>
   );
