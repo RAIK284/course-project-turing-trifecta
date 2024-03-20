9deee39d4b2cf9b8a3892759174fa38ddba9e06b
nbray03 <112013865+nbray03@users.noreply.github.com>
2024-03-19 22:26:50 -0500

    Styling Changes to the frontend.

--- wavelength/src/pages/AuthenticatedLandingPage.tsx
+++ wavelength/src/pages/AuthenticatedLandingPage.tsx
@@ -14,45 +14,48 @@ const AuthenticatedLandingPage: React.FC = () => {
   };
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-1/3 my-auto space-y-4 flex flex-col items-center">
-        <div className="w-full text-center">
-          <text
-            className="text-2xl font-regular text-center"
-            style={{ textShadow: "0 0 5px #ffffff" }}
-          >
-            JOIN SESSION
-          </text>
-          <input
-            type="text"
-            placeholder="Enter Game Session ID"
-            className="w-full h-10 p-2 bg-cover-blue rounded-lg text-white text-center placeholder-white"
-          />
-        </div>
-        <div className="w-full flex items-center">
-          <hr className="flex-grow border-t border-gray-300" />
-          <span className="mx-2 text-gray-500 font-bold">or</span>
-          <hr className="flex-grow border-t border-gray-300" />
-        </div>
-        <div className="w-full">
+    <div className="h-screen flex flex-col justify-between items-center overflow-y-auto">
+    <div className="w-1/3 my-auto space-y-4 flex flex-col items-center">
+      <div className="w-full text-center">
+        <text
+          className="text-2xl font-regular text-center"
+          style={{ textShadow: "0 0 5px #ffffff" }}
+        >
+          JOIN SESSION
+        </text>
+        <input
+          type="text"
+          placeholder="ENTER GAME SESSION ID"
+          className="w-full h-10 p-2 bg-cover-blue rounded-lg text-white text-center placeholder-white"
+        />
+      </div>
+      <div className="w-full flex items-center">
+        <hr className="flex-grow border-t-2 border-gray-300" />
+        <span className="mx-4 text-gray-500 font-bold text-2xl"
+        style={{ textShadow: '0 0 5px #ffffff' }}
+        >or</span>
+        <hr className="flex-grow border-t-2 border-gray-300" />
+      </div>
+      <div className="w-full">
+        <button
+          className="w-full h-10 p-2 bg-target-2 rounded-lg text- text-center"
+          onClick={createSession}
+        >
+          CREATE SESSION
+        </button>
+        <div className="absolute bottom-5 left-5">
           <button
-            className="w-full h-10 p-2 bg-target-2 rounded-lg text-black text-center"
-            onClick={createSession}
+            onClick={handleRulesPageButtonClick}
+            className="w-full h-10 text-bold p-2 bg-target-3 rounded-lg text-white text-center"
+            style={{ textShadow: '0 0 5px #ffffff' }}
           >
-            CREATE SESSION
+            HOW TO PLAY
           </button>
-          <div className="absolute bottom-5 left-5">
-            <button
-              onClick={handleRulesPageButtonClick}
-              className="w-full h-10 p-2 bg-target-3 rounded-lg text-black text-center"
-            >
-              HOW TO PLAY
-            </button>
-          </div>
         </div>
       </div>
-      <Spinner />
     </div>
+    <div className = "fixed bottom-100 left-100 w-half"><Spinner /></div> {/* Adjust this value as needed */}
+  </div>
   );
 };
 
