c93881c6a38d8bfa8f0390acc2a4105bfcedbe68
jakeRance <jake.rance01@gmail.com>
2024-04-18 22:02:12 -0500

    Made the register page a form

--- wavelength/src/pages/RegisterPage.tsx
+++ wavelength/src/pages/RegisterPage.tsx
@@ -1,19 +1,31 @@
 import { useNavigate } from "react-router";
 import { WavelengthPath } from "../routing/Routes";
+import User from "../models/User";
 import React, { ChangeEvent, useState } from 'react';
 // import {account} from "../api/api"
+import { useStore } from "../stores/store";
+import { useStoreValue } from "../stores/storeValue";
 
 
 const RegisterPage: React.FC = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [confirm, setConfirm] = useState("")
+  const { userStore } = useStore();
+  const { register, userStoreValue } = userStore;
+  const [, isLoading, error] = useStoreValue(userStoreValue);
+
 
 
 
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
+  };
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
   };
 
   const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -25,6 +37,7 @@ const RegisterPage: React.FC = () => {
   }
 
   function handleRegisterClick(){
+    //Still need to connect to backend and actually register the user
     if(password == confirm){
       navigate(WavelengthPath.LANDING);
     }
@@ -33,13 +46,46 @@ const RegisterPage: React.FC = () => {
 
   return(
     <div className="h-screen flex justify-center self-center space-y-1/2">
-      <div className="grid grid-rows-4 gap-y-6 h-1/2 w-1/3 my-auto">
-        <input className="bg-theme-blue border-2 border-target-2 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" placeholder="EMAIL" onChange={handleEmailChange}></input>
-        <input className="bg-theme-blue border-2 border-target-4 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" placeholder="USERNAME" onChange={handleEmailChange}></input>
-        <input className="bg-theme-blue border-2 border-target-3 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" placeholder="PASSWORD" min={8} onChange={handlePasswordChange}></input>
-        <input className="bg-theme-blue border-2 border-target-3 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" placeholder="CONFIRM PASSWORD" min={8} onChange={handleConfirmChange}></input>
-        <button className="bg-cover-blue h-11 rounded-lg md:text-2xl" onClick={handleRegisterClick} style={{textShadow: "0 0 5px #ffffff"}}>REGISTER</button>
-      </div>
+      <form className="grid grid-rows-4 gap-y-6 h-1/2 w-1/3 my-auto">
+
+        <input //Enter Email
+        className="bg-theme-blue border-2 border-target-2 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" 
+        placeholder="EMAIL" 
+        onChange={handleEmailChange}>
+        </input>
+
+        <input //Enter Username
+        className="bg-theme-blue border-2 border-target-4 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" 
+        placeholder="USERNAME" 
+        onChange={handleUsernameChange}>
+        </input>
+
+        <input //Enter Password
+        className="bg-theme-blue border-2 border-target-3 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" 
+        placeholder="PASSWORD" 
+        min={8} 
+        onChange={handlePasswordChange}>
+        </input>
+
+        <input //Confirm Password
+        className="bg-theme-blue border-2 border-target-3 text-center w-full h-10 p-2 rounded-lg placeholder-white text-white" 
+        placeholder="CONFIRM PASSWORD" 
+        min={8} 
+        onChange={handleConfirmChange}>
+        </input>
+
+
+        <button 
+        type="submit" 
+        className="bg-cover-blue h-11 rounded-lg md:text-2xl" 
+        disabled={isLoading || !email || !username || !password || !confirm}
+        onClick={handleRegisterClick} 
+        style={{textShadow: "0 0 5px #ffffff"}}>
+        REGISTER
+        </button>
+        
+        {error && <span className="text-center-red">{error}</span>}
+      </form>
     </div>
   );
 };
